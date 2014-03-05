4193fcc87bd911e498eb8783006d70f8ffc88259
Mario Pierre-Louis Marín <b.mplm@tid.es>
2014-03-05 11:53:39 +0100

    Not connected to the NetworkPolicies API

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	var miArray = new Array(3)
+
+miArray[0] = 155
+miArray[1] = 4
+miArray[2] = 499
+
+for (i=0;i<3;i++){
+    document.write("Posición " + i + " del array: " + miArray[i])
+    document.write("<br>")
+}
+});
